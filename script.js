5defe59762ed35079944fcea1360c34dfdbb3c41
kamesh-p <kkamesh4790@gmail.com>
2023-02-18 00:08:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const adviceHeader = document.getElementById("advice-header");
+const adviceText = document.getElementById("advice-text");
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  getAdvice();
+});
+window.onload = () => {
+  getAdvice();
+};
+function getAdvice() {
+  fetch(`https://api.adviceslip.com/advice`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((adviseInfo) => {
+      const advise1 = adviseInfo.slip;
+      adviceHeader.innerHTML = `ADVICE# ${advise1.id}`;
+      adviceText.innerHTML = `"${advise1.advice}".`;
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+}
